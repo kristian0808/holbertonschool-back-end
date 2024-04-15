5a17d8ad66cc3f7fdd803952f43360b5f09176a7
kristian0808 <kristianlleshaj@hotmail.com>
2024-04-16 01:04:30 +0200

    added comments

--- 0-gather_data_from_an_API.py
+++ 0-gather_data_from_an_API.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
+"""
+This script fetches todo tasks for a given employee id from the JSON
+Placeholder API and prints the completed tasks and their titles.
+
+Usage:
+    python3 0-gather_data_from_an_API.py <employee_id>
+
+Args:
+    employee_id (int): The id of the employee whose tasks to fetch.
+
+Example:
+    python3 0-gather_data_from_an_API.py 1
+"""
 
 import requests
 import sys
